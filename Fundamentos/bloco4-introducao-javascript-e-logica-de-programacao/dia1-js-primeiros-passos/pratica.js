de74e7326da7ddbff05dcd704f8aef3560d4806d
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-25 20:10:58 -0300

    Added parts 1 2 & 3

---
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia1-js-primeiros-passos/pratica.js
@@ -0,0 +1,30 @@
+const a = 7;
+const b = 9;
+
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
+
+const n1 = 6
+const n2 = 4
+const n3 = 8
+
+
+if (n1 > n2) {
+    console.log(n1)
+}
+else {
+    console.log(n2)
+}
+
+if (n1 > n2 && n1 > n3) {
+    console.log(n1)
+}
+else if (n2 > n1 && n2 > n3) {
+    console.log(n2)
+}
+else {
+    console.log(n3)
+}
