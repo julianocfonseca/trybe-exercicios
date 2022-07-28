bdf2c13938b20e9c7c5608baa6d69dae66e8dab7
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-28 15:25:07 -0300

    Added exercise 7

--- Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia1-js-primeiros-passos/pratica.js
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia1-js-primeiros-passos/pratica.js
@@ -73,3 +73,23 @@ if (result === 'bishop') {
 } else if (result === 'rook') {
     console.log('move forward, backward or sideways, but cannot move diagonally')
 }
+
+// Exercício 7:
+
+let nota = 99;
+
+if (nota < 0 || nota > 100) {
+    console.log('Error')
+} else if (nota >= 90) {
+    console.log('A')
+} else if (nota >= 80) {
+    console.log('B')
+} else if (nota >= 70) {
+    console.log('C')
+} else if (nota >= 60) {
+    console.log('D')
+} else if (nota >= 50) {
+    console.log('E')
+} else if (nota < 50) {
+    console.log('F')
+}
