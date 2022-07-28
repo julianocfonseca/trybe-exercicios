a8223db213b2a0534993bafe7ad17ea705266221
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-28 15:31:26 -0300

    Added exercise 8

--- Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia1-js-primeiros-passos/pratica.js
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia1-js-primeiros-passos/pratica.js
@@ -93,3 +93,13 @@ if (nota < 0 || nota > 100) {
 } else if (nota < 50) {
     console.log('F')
 }
+
+// Exercício 8:
+
+const number1 = 5;
+const number2 = 6;
+const number3 = 3;
+
+if (number1 % 2 === 0 || number2 % 2 === 0 || number3 % 2 ===0) {
+    console.log('true')
+} else {console.log('false')}
