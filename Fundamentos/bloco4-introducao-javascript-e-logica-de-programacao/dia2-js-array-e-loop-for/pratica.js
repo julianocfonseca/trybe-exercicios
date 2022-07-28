6ea79362196e1485149bfe8174e8856c17e00135
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-28 18:16:28 -0300

    Addex incomplete exercise 2

--- Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia2-js-array-e-loop-for/pratica.js
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia2-js-array-e-loop-for/pratica.js
@@ -5,3 +5,10 @@ let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 for (let numeros of numbers) {
     console.log(numeros)
 }
+
+// Exercício 2:
+
+for (let index = 0;index < numbers.length;index +=1) {
+    let soma = number[index] + 0;
+    console.log(soma)
+}
