28d9b126c8c670a5cf7a454c72924c23beac148a
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-30 21:22:42 -0300

    Added exercise 3

---
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia3-logica-de-programacao-e-algoritmos/Exercicio3.js
@@ -0,0 +1,15 @@
+let array = ['java', 'javascript', 'python', 'html', 'css'];
+let maiorPalavra = array[0];
+let menorPalavra = array[0];
+
+for (let index = 1;index < array.length;index += 1) {
+    if (array[index].length > maiorPalavra.length) {
+        maiorPalavra = array[index];
+    }
+    if (array[index].length < menorPalavra.length) {
+        menorPalavra = array[index];
+    }
+}
+
+console.log(maiorPalavra);
+console.log(menorPalavra);
