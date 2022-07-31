5c41103a29efc36b58aa6f1a53eef147ef528940
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-07-30 22:36:56 -0300

    Added functions exercise

---
+++ Fundamentos/bloco4-introducao-javascript-e-logica-de-programacao/dia4-objetos-e-funcoes/fixacao3.js
@@ -0,0 +1,50 @@
+function soma(a,b) {
+    return a + b;
+}
+
+function subtracao(a,b) {
+    return a - b;
+}
+
+function multiplicacao(a,b) {
+    return a * b;
+}
+
+function divisao(a,b) {
+    return a / b;
+}
+
+function modulacao(a,b) {
+    return a % b;
+}
+
+function maiorNumero(x,y) {
+    if (x > y) {
+        return x;
+    } return y;
+}
+
+function maiorDosNumeros(x,y,z) {
+    if (x > y && x > z) {
+        return x
+    } if (y > x && y > z) {
+        return y
+    } return z
+}
+
+function positiveNegative(x) {
+    if (x > 0) {
+        return 'positive'
+    } if (x < 0) {
+        return 'negative'
+    } return 'zero'
+}
+
+function triAngles(x,y,z) {
+    if (x + y + z === 180 ) {
+      return 'true'
+    } return 'false'
+}
+
+
+
