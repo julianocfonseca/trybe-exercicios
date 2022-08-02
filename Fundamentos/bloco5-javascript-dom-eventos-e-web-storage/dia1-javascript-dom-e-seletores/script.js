79f38dfcd5298a34d129f1f82bfb897120e89ed6
Juliano Fonseca <julianof1796@yahoo.com.br>
2022-08-02 19:14:05 -0300

    Added fixation exercise 3 & customization files

---
+++ Fundamentos/bloco5-javascript-dom-eventos-e-web-storage/dia1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,9 @@
+document.getElementById('header-container').style.backgroundColor = 'green'
+document.getElementsByClassName('emergency-tasks')[0].style.backgroundColor = 'pink'
+document.querySelector('h3').style.backgroundColor = 'purple'
+document.querySelectorAll('h3')[1].style.backgroundColor = 'purple'
+document.getElementsByClassName('no-emergency-tasks')[0].style.backgroundColor = 'orange'
+document.querySelectorAll('h3')[2].style.backgroundColor = 'black'
+document.querySelectorAll('h3')[3].style.backgroundColor = 'black'
+document.getElementById('footer-container').style.backgroundColor = 'gray'
+
